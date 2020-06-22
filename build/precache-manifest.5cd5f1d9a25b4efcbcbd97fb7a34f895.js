340c90effcf6551219281fe9dbb838e91c7ccd1d
Malcolm Nunley <Mxnunley1@gmail.com>
2020-06-22 12:33:37 -0400

    created a build for app

---
+++ build/precache-manifest.5cd5f1d9a25b4efcbcbd97fb7a34f895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "330b92d78f192b99c60c27ff031a904c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27b06ba513cd60912371",
+    "url": "/static/css/main.ef8ac0f4.chunk.css"
+  },
+  {
+    "revision": "7f9aba0b058819024249",
+    "url": "/static/js/2.f3c31ea1.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.f3c31ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b06ba513cd60912371",
+    "url": "/static/js/main.39b2922e.chunk.js"
+  },
+  {
+    "revision": "ef7fff37e907797dcad9",
+    "url": "/static/js/runtime-main.eb43824e.js"
+  }
+]);
